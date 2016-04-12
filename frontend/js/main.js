c92260f5942493e24d3f4ecf4b811c34e9216dfb
geo2a <georgiylukjanov@gmail.com>
2016-04-12 17:19:22 +0300

    Login form added.

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,99 +1,110 @@
 $(document).ready(function() {
-    // Ask server for array of students data
-    $.ajax({ 
-        type: "GET",
-        url: "http://127.0.0.1:8083/files",
-        beforeSend: 
-            function (xhr) {
-                xhr.setRequestHeader(
-                    'Authorization', 
-                    "Basic " + btoa("servant" + ":" + "server")
-                );
-            },
-        error: 
-            function(jqXHR, textStatus, errorThrown) {
-                switch(jqXHR.status) {
-                    case 500:
-                        $("#500-warning").show();
-                        break;
-                    case 401: 
-                        $("#401-warning").show();
-                        break;
-                    default:
-                        console.log(textStatus + "," + errorThrown + "," + jqXHR.responseText);
-                        console.log(arguments);
-                        $("#unknown-error-warning").show();
-                }
-            },
-        success: 
-            function(result) {
-                var sourceFiles = result;
-                // calculate unique user ids
-                var uids = _.chain(sourceFiles)
-                    .map(function(file) {
-                        return file.uid;
-                    })
-                    .uniq()
-                    .sortBy()
-                    .value();
-                
-                if (uids.length == 0) {
-                    $("#no-students-warning").show();
-                } else {
-                    var groupedFiles = _.chain(sourceFiles)
-                        .groupBy(function(x) {return x.uid;})
+    $("#login-button").on('click', function(e) { 
+        var username = $("#login-username").val();
+        var password = $("#login-password").val();
+        
+        // Authenticate teacher and ask server for array of students data
+        $.ajax({ 
+            type: "GET",
+            url: "http://127.0.0.1:8083/files",
+            beforeSend: 
+                function (xhr) {
+                    xhr.setRequestHeader(
+                        'Authorization', 
+                        "Basic " + btoa(username + ":" + password)
+                    );
+                },
+            error: 
+                function(jqXHR, textStatus, errorThrown) {
+                    switch(jqXHR.status) {
+                        case 500:
+                            $("#500-warning").show();
+                            break;
+                        case 401: 
+                            $("#401-warning").show();
+                            break;
+                        default:
+                            console.log(textStatus + "," + 
+                                        errorThrown + "," + 
+                                        jqXHR.responseText);
+                            console.log(arguments);
+                            $("#unknown-error-warning").show();
+                    }
+                },
+            success: 
+                function(result) {
+                    // hide login form
+                    $("#login-form").hide();
+
+                    var sourceFiles = result;
+                    // calculate unique user ids
+                    var uids = _.chain(sourceFiles)
+                        .map(function(file) {
+                            return file.uid;
+                        })
+                        .uniq()
+                        .sortBy()
                         .value();
-
-                    // build students list on the ui
-                    var studentTabs = document.getElementById("tabs");
-                    var studentTabsContents = document.getElementById("tabs-contents");
-
-                    _.forEach(uids, function(uid) {
-                        // students tabs list
-                        var studentTab = document.createElement("li");
-                        var a = document.createElement('a');
-                        a.href =  "#tab-" + uid;  
-                        a.innerHTML = "Student " + uid; 
-                        studentTab.appendChild(a);
-                        studentTabs.appendChild(studentTab);
-
-                        // students tabs contents
-                        var tabContent = document.createElement("div");
-                        tabContent.id = "tab-" + uid;
-                        tabContent.className = "tab-content";
-                        var filesOfThisUser = _.chain(groupedFiles[uid])
-                            .map(function (x) {
-                                return x.file;})
-                            .map(sourceFileJSONtoDOM)
-                            .map(function(fileNode) {
-                                var li = document.createElement("li");
-                                li.appendChild(fileNode);
-                                return li;
-                            })
+                    
+                    if (uids.length == 0) {
+                        $("#no-students-warning").show();
+                    } else {
+                        var groupedFiles = _.chain(sourceFiles)
+                            .groupBy(function(x) {return x.uid;})
                             .value();
-                        var ul = document.createElement("ul");
-                        _.forEach(filesOfThisUser, function(li) {
-                            ul.appendChild(li);
-                        });
-                        tabContent.appendChild(
-                            ul
-                        );
-                        studentTabsContents.appendChild(tabContent);
-                    });
 
-                    $(".tabs-menu a").click(function(event) {
-                        $(".tab").css("display", "block");
-                        event.preventDefault();
-                        $(this).parent().addClass("current");
-                        $(this).parent().siblings().removeClass("current");
-                        var tab = $(this).attr("href");
-                        $(".tab-content").not(tab).css("display", "none");
-                        $(tab).fadeIn();
-                    });
+                        // build students list on the ui
+                        var studentTabs = document.getElementById("tabs");
+                        var studentTabsContents = 
+                            document.getElementById("tabs-contents");
+
+                        _.forEach(uids, function(uid) {
+                            // students tabs list
+                            var studentTab = document.createElement("li");
+                            var a = document.createElement('a');
+                            a.href =  "#tab-" + uid;  
+                            a.innerHTML = "Student " + uid; 
+                            studentTab.appendChild(a);
+                            studentTabs.appendChild(studentTab);
+
+                            // students tabs contents
+                            var tabContent = document.createElement("div");
+                            tabContent.id = "tab-" + uid;
+                            tabContent.className = "tab-content";
+                            var filesOfThisUser = _.chain(groupedFiles[uid])
+                                .map(function (x) {
+                                    return x.file;})
+                                .map(sourceFileJSONtoDOM)
+                                .map(function(fileNode) {
+                                    var li = document.createElement("li");
+                                    li.appendChild(fileNode);
+                                    return li;
+                                })
+                                .value();
+                            var ul = document.createElement("ul");
+                            _.forEach(filesOfThisUser, function(li) {
+                                ul.appendChild(li);
+                            });
+                            tabContent.appendChild(
+                                ul
+                            );
+                            studentTabsContents.appendChild(tabContent);
+                        });
+
+                        $(".tabs-menu a").click(function(event) {
+                            $(".tab").css("display", "block");
+                            event.preventDefault();
+                            $(this).parent().addClass("current");
+                            $(this).parent().siblings().removeClass("current");
+                            var tab = $(this).attr("href");
+                            $(".tab-content").not(tab).css("display", "none");
+                            $(tab).fadeIn();
+                        });
+                    }
                 }
-            }
-    }
-    );
+        }
+        );
+    });
 });
 
 function sourceFileJSONtoDOM(file) {
