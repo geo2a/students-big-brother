6b90af88f9ac7e9e027749a1d20577e296814b22
geo2a <georgiylukjanov@gmail.com>
2016-05-08 15:00:40 +0300

    Auto redrawing of teachers table on adding new teacher.

--- frontend/js/admin.js
+++ frontend/js/admin.js
@@ -1,4 +1,6 @@
-$(document).ready(function() {
+$(document).ready(main);
+
+function main() {
   $.ajax({
     type: "GET",
     url: "http://127.0.0.1:8083/admin/list-teachers",
@@ -12,27 +14,31 @@ $(document).ready(function() {
         var teachers = result;
         drawTable(teachers);
         $("#add-teacher-button").on('click', function(e) {
-          var newTeacher = {username:"lal1",password:"lal2"};
-          console.log(newTeacher);
+          var uname = $("#new-teacher-username").val();
+          var pwd = $("#new-teacher-password").val();
+          if (isInvalid(uname) || isInvalid(pwd)) {
+            alert("Username or Password is invalid!");
+            return;
+          }
+          var newTeacher = { username:uname
+                           , password:pwd
+                           };
           $.ajax({
             type: "POST",
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json'
             },
+            success: function(justAddedTeacher) {
+              drawRow(justAddedTeacher);
+            },
             url: "http://127.0.0.1:8083/admin/register-teacher",
             data: JSON.stringify(newTeacher),
-            success: function(data, textStatus, jqXHR) {
-              console.log("lalki");
-            },
-            error: function(jqXHR, textStatus, errorThrown) {
-              console.log(errorThrown);
-            }
           });
         });
       }
     });
-});
+}
 
 function drawTable(data) {
     for (var i = 0; i < data.length; ++i) {
@@ -43,11 +49,18 @@ function drawTable(data) {
 
 function drawRow(rowData) {
     var row = $("<tr />")
-    $("#teachers-list-table").append(row); //this will append tr element to table... keep its reference for a while since we will add cels into it
+    // console.log($("#teachers-list-table:last"));
+    // $("#teachers-list-table").append(row); //this will append tr element to table... keep its reference for a while since we will add cels into it
     row.append($("<td>" + rowData.teacher_id + "</td>"));
     row.append($("<td>" + rowData.teacher_credential.username + "</td>"));
     row.append($("<td>" + rowData.teacher_credential.password + "</td>"));
     row.append(
-      $("<td><button class=\"teacher-delete-button\">Delete</button></td>")
+      $("<td><button class=\"delete-button\">Delete</button></td>")
     );
+    $("#teachers-list-table").append(row);
+}
+
+function isInvalid(input) {
+  input = input.trim();
+  return (!input || input.length === 0);
 }
