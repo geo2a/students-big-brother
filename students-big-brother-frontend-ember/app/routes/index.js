0500b259e7d0c2807a2be780452d4a95d87cbb62
geo2a <georgiylukjanov@gmail.com>
2016-07-29 15:32:18 +0300

    Initial integration of new (ember.js) implementation of frontend and backend.

--- students-big-brother-frontend-ember/app/routes/index.js
+++ students-big-brother-frontend-ember/app/routes/index.js
@@ -2,6 +2,22 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    return  Ember.$.getJSON('/student-source-files');
+    const username = "AlanTuring"
+    const password = "123"
+    const hostname = "localhost"
+    const fetchOptions = { method: "GET"
+                         , headers: { 'Accept': 'application/json'
+                                    , "Authorization": "Basic " +
+                                       btoa(username + ":" + password)
+                                    }
+                         , mode: "cors"
+                         }
+    const errorHandler = error => {
+      console.log(error)
+    }
+    return fetch("http://" + hostname + ":8083/files", fetchOptions)
+                .then(response => response.json())
+                .catch(errorHandler)
+    // return  Ember.$.getJSON('/student-source-files');
   }
 });
--- students-big-brother-frontend-ember/app/routes/students.js
+++ students-big-brother-frontend-ember/app/routes/students.js
@@ -8,9 +8,25 @@ export default Ember.Route.extend({
     }
   },
   model(params) {
+    const username = "AlanTuring"
+    const password = "123"
+    const hostname = "localhost"
+    const fetchOptions = { method: "GET"
+                         , headers: { 'Accept': 'application/json'
+                                    , "Authorization": "Basic " +
+                                       btoa(username + ":" + password)
+                                    }
+                         , mode: "cors"
+                         }
+    const errorHandler = error => {
+      console.log(error)
+    }
+
     return Ember.RSVP.hash({
-      currentStudent: Ember.$.getJSON('/student-source-files?sid=' + params.student_id),
-      allStudents: Ember.$.getJSON('/student-source-files')
+      // currentStudent: Ember.$.getJSON('/student-source-files?sid=' + params.student_id),
+      allStudents: fetch("http://" + hostname + ":8083/files", fetchOptions)
+                        .then(response => response.json())
+                        .catch(errorHandler)
     });
   }
 });
